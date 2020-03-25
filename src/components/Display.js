a6480b2b22333c80318ef550b2f8e6301e816437
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-03-25 15:28:27 -0400

    Implemented basic components

---
+++ src/components/Display.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Display = ({ result }) => <input className="Display" value={result} />;
+
+Display.propTypes = {
+  result: PropTypes.string.isRequired,
+};
+
+export default Display;
