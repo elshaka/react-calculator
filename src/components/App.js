35cdc3beec9b1d1300f7ccf9836f646a49d7eec8
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-03-26 10:21:44 -0400

    Added default value for Display result prop

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import ButtonPanel from './ButtonPanel';
 function App() {
   return (
     <div className="App">
-      <Display result={0} />
+      <Display />
       <ButtonPanel />
     </div>
   );
--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types';
 const Display = ({ result }) => <input className="Display" value={result} />;
 
 Display.propTypes = {
-  result: PropTypes.string.isRequired,
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
 };
 
 export default Display;
