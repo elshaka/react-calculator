6e10af8b05ce44db801d5ae516d52bb0785f243c
Eleazar Meza <bairesdev_emeza@iseatz.com>
2020-04-01 12:09:33 -0400

    Removed iffy undefined thingy in Display container

--- src/containers/Display.js
+++ src/containers/Display.js
@@ -5,15 +5,11 @@ import { connect } from 'react-redux';
 const Display = ({ result }) => <div className="Display">{result}</div>;
 
 Display.propTypes = {
-  result: PropTypes.string,
-};
-
-Display.defaultProps = {
-  result: '0',
+  result: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = state => ({
-  result: state.next || state.total || undefined,
+  result: state.next || state.total || '0',
 });
 
 export default connect(mapStateToProps)(Display);
